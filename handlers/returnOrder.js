261a1d3828c741a1886d2f8b61a1a16ee9bdabc5
Jessica Deacon <j.r.deacon@hotmail.co.uk>
2021-01-22 10:44:07 +0000

    write returnOrder function

--- handlers/returnOrder.js
+++ handlers/returnOrder.js
@@ -1,9 +1,26 @@
 const db = require("./database/connection");
 
-function returnOrder(req, res) {
-  db.query("SELECT * FROM return_order").then((res) => {
-    console.log("result", res);
-  });
-  res.writeHead(200, { "content-type": "text/html" });
-  res.end("<h1>Hello</h1>");
-}
+// function returnOrder(req, res) {
+//   db.query("SELECT * FROM return_order").then((res) => {
+//     console.log("result", res);
+//   });
+//   res.writeHead(200, { "content-type": "text/html" });
+//   res.end("<h1>Hello</h1>");
+// }
+
+const returnOrder = (req, res, next) => {
+  const id = req.return_order.id;
+  const order_number = req.return_order.order_number;
+  const quantity = req.return_order.quantity;
+  const reason = req.return_order.reason;
+
+  db(order_number, quantity, reason)
+    .then(() => {
+      res.status(201).send({
+        message: "returns generated",
+      });
+    })
+    .catch(next);
+};
+
+module.exports = returnOrder;
